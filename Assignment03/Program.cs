034b20a861b8add501d45d2e38fdd4295cd65a55
moohammedddd <midorafat115566@gmail.com>
2025-02-08 18:25:14 +0200

    Q1

--- Assignment03/Program.cs
+++ Assignment03/Program.cs
@@ -2,9 +2,40 @@
 {
     internal class Program
     {
+        public static Stack<int> Reverse(Queue<int> Que)
+        {
+            Stack<int> Stack = new Stack<int>();
+            //Stack.Push(Que.Dequeue());
+            //Stack.Push(Que.Dequeue());
+            //Stack.Push(Que.Dequeue());
+            while (Que.Count > 0)
+            {
+                Stack.Push(Que.Dequeue());
+            }
+
+
+            return Stack;
+        }
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            #region Q1  Assignment 02 implement a function to reverse the elements of a queue using a stack.Given a Queue,
+            Queue<int> Que = new Queue<int>();
+            Que.Enqueue(1);
+            Que.Enqueue(2);
+            Que.Enqueue(3);
+            Console.WriteLine("Before Reversed");
+            foreach (var item in Que)
+            {
+                Console.WriteLine(item);
+            }
+            Stack<int> Reversed = Reverse(Que);
+            Console.WriteLine("after Reversed");
+            foreach (int i in Reversed)
+            {
+                Console.WriteLine(i);
+            }
+
+            #endregion
         }
     }
 }
